1f76839c37afaf48ab5d4f2751afe0e49e42b08a
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-01-12 15:38:54 -0800

    2000 analysis v1

--- Infutor+Census_2000_Analysis.py
+++ Infutor+Census_2000_Analysis.py
@@ -46,9 +46,16 @@ df_census = pd.read_excel(
         'SE_T002_001',  # total population (again)
         'SE_T002_002',  # population density
         'SE_T167_001',  # median gross rent
+        'SE_T093_003',  # median renter incomes
     ],
     skiprows=1
 )
+
+se_high_loss_areas = pd.read_pickle(
+    './data/infutor+census/se_high_loss_areas.pkl'
+)
+
+df_high_loss_areas = df_census[df_census['Geo_FIPS'].isin(se_high_loss_areas)]
 
 
 ### Analysis ###
@@ -58,6 +65,10 @@ if verbose:
 
 def years_to_effdate(beginning, end):
     return (100 * beginning + 1), (100 * end + 12)
+
+def census_col_by_area(name, df=df_census):
+    se = df[['Geo_FIPS', name]].set_index('Geo_FIPS').squeeze()
+    return se
 
 def count_moves(moves):
     return moves.shape[0]
@@ -87,7 +98,7 @@ area_results = {}
 
 area_results[
     "Total population at beginning of the period"
-] = df_high_loss_areas[f'totalpop{census_year}'].sum()
+] = df_high_loss_areas['SE_T001_001'].sum()
 
 
 area_results[
@@ -153,7 +164,7 @@ area_results[
 ] = df_moves.loc[:, 'high-loss', 'LA/OC'] \
             .groupby('dest_fips') \
             .size() \
-            .mul(census_col_by_area(f'popdens{census_year}')) \
+            .mul(census_col_by_area('SE_T002_002')) \
             .sum() \
     / count_moves(df_moves.loc[:, 'high-loss', 'LA/OC'])
 
@@ -161,25 +172,25 @@ area_results[
     "Median density of destination tracts for moves that end in LA and "
     "Orange County but are not in high-loss decile"
 ] = df_moves.loc[:, 'high-loss', 'LA/OC']['dest_fips'] \
-            .map(census_col_by_area(f'popdens{census_year}')) \
+            .map(census_col_by_area('SE_T002_002')) \
             .median()
 
-area_results[
-    "Weighted average ridership of destination tracts for moves that end "
-    "in LA and Orange County but are not in high-loss decile"
-] = df_moves.loc[:, 'high-loss', 'LA/OC'] \
-            .groupby('dest_fips') \
-            .size() \
-            .mul(census_col_by_area(f'boardings{census_year}')) \
-            .sum() \
-    / count_moves(df_moves.loc[:, 'high-loss', 'LA/OC'])
+# area_results[
+#     "Weighted average ridership of destination tracts for moves that end "
+#     "in LA and Orange County but are not in high-loss decile"
+# ] = df_moves.loc[:, 'high-loss', 'LA/OC'] \
+#             .groupby('dest_fips') \
+#             .size() \
+#             .mul(census_col_by_area(f'boardings{census_year}')) \
+#             .sum() \
+#     / count_moves(df_moves.loc[:, 'high-loss', 'LA/OC'])
 
-area_results[
-    "Median ridership of destination tracts for moves that end in LA "
-    "and Orange County but are not in high-loss decile"
-] = df_moves.loc[:, 'high-loss', 'LA/OC']['dest_fips'] \
-            .map(census_col_by_area(f'boardings{census_year}')) \
-            .median()
+# area_results[
+#     "Median ridership of destination tracts for moves that end in LA "
+#     "and Orange County but are not in high-loss decile"
+# ] = df_moves.loc[:, 'high-loss', 'LA/OC']['dest_fips'] \
+#             .map(census_col_by_area(f'boardings{census_year}')) \
+#             .median()
 
 
 area_results[
@@ -191,7 +202,7 @@ area_results[
         df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']],
         lambda orig, dest: dest < orig,
         lambda _: 9360,
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002')
     )
 ) / count_moves(df_moves.loc[False, 'high-loss', :])
 
@@ -204,7 +215,7 @@ area_results[
         df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']],
         lambda orig, dest: dest < orig,
         lambda _: 9360,
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002')
     )
 ) / count_moves(df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']])
 
@@ -218,7 +229,7 @@ area_results[
         df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']],
         lambda orig, dest: dest < orig,
         lambda _: 16_700,
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002')
     )
 ) / count_moves(df_moves.loc[False, 'high-loss', :])
 
@@ -231,7 +242,7 @@ area_results[
         df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']],
         lambda orig, dest: dest < orig,
         lambda _: 16_700,
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002')
     )
 ) / count_moves(df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']])
 
@@ -244,8 +255,8 @@ area_results[
     filter_moves_using_fips(
         df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']],
         lambda orig, dest: dest < orig,
-        census_col_by_area(f'popdens{census_year}'),
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002'),
+        census_col_by_area('SE_T002_002')
     )
  ) / count_moves(df_moves.loc[False, 'high-loss', :])
 
@@ -257,8 +268,8 @@ area_results[
     filter_moves_using_fips(
         df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']],
         lambda orig, dest: dest < orig,
-        census_col_by_area(f'popdens{census_year}'),
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002'),
+        census_col_by_area('SE_T002_002')
     )
 ) / count_moves(df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']])
 
@@ -268,9 +279,9 @@ area_results[
     "tract that end in LA or OC"
 ] = (
     df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']]['dest_fips'] \
-            .map(census_col_by_area(f'popdens{census_year}')) \
+            .map(census_col_by_area('SE_T002_002')) \
     - df_moves.loc[False, 'high-loss', ['high-loss', 'LA/OC']]['orig_fips'] \
-              .map(census_col_by_area(f'popdens{census_year}'))
+              .map(census_col_by_area('SE_T002_002'))
 ).mean()
 
 
@@ -295,7 +306,7 @@ area_results[
 ] = df_moves.loc[False, :, 'high-loss'] \
             .groupby('orig_fips') \
             .size() \
-            .mul(census_col_by_area(f'popdens{census_year}')) \
+            .mul(census_col_by_area('SE_T002_002')) \
             .sum() \
     / count_moves(df_moves.loc[False, :, 'high-loss'])
 
@@ -306,7 +317,7 @@ area_results[
 ] = df_moves.loc[:, 'LA/OC', 'high-loss'] \
             .groupby('orig_fips') \
             .size() \
-            .mul(census_col_by_area(f'popdens{census_year}')) \
+            .mul(census_col_by_area('SE_T002_002')) \
             .sum() \
     / count_moves(df_moves.loc[:, 'LA/OC', 'high-loss'])
 
@@ -314,7 +325,7 @@ area_results[
     "Median density of tracts where in-moves originated for all moves that "
     "aren't within the exact same tract"
 ] = df_moves.loc[False, :, 'high-loss']['orig_fips'] \
-            .map(census_col_by_area(f'popdens{census_year}')) \
+            .map(census_col_by_area('SE_T002_002')) \
             .median()
 
 area_results[
@@ -322,7 +333,7 @@ area_results[
     "originate outside the high-loss tracts but that don't originate "
     "outside LA or Orange County"
 ] = df_moves.loc[:, 'LA/OC', 'high-loss']['orig_fips'] \
-            .map(census_col_by_area(f'popdens{census_year}')) \
+            .map(census_col_by_area('SE_T002_002')) \
             .median()
 
 
@@ -332,8 +343,8 @@ area_results[
     filter_moves_using_fips(
         df_moves.loc[False, :, 'high-loss'],
         lambda orig, dest: orig < dest,
-        census_col_by_area(f'popdens{census_year}'),
-        census_col_by_area(f'popdens{census_year}')
+        census_col_by_area('SE_T002_002'),
+        census_col_by_area('SE_T002_002')
     )
 ) / count_moves(df_moves.loc[False, :, 'high-loss'])
 
@@ -343,9 +354,9 @@ area_results[
     "tract"
 ] = (
     df_moves.loc[False, :, 'high-loss']['dest_fips'] \
-            .map(census_col_by_area(f'popdens{census_year}')) \
+            .map(census_col_by_area('SE_T002_002')) \
     - df_moves.loc[False, :, 'high-loss']['orig_fips'] \
-              .map(census_col_by_area(f'popdens{census_year}'))
+              .map(census_col_by_area('SE_T002_002'))
 ).mean()
 
 
@@ -386,19 +397,27 @@ area_results[
 area_results[
     "An overall rate of outmigration from high-loss tracts"
 ] = count_moves(df_moves.loc[False, 'high-loss', :]) \
-    / df_high_loss_areas[f'totalpop{census_year}'].sum()
+    / df_high_loss_areas['SE_T001_001'].sum()
 
 area_results[
     "An overall rate of in-migration to high-loss tracts"
 ] = count_moves(df_moves.loc[False, :, 'high-loss']) \
-    / df_high_loss_areas[f'totalpop{census_year}'].sum()
+    / df_high_loss_areas['SE_T001_001'].sum()
+
+area_results[
+    "Average median gross rent"
+] = df_high_loss_areas['SE_T167_001'].mean()
+
+area_results[
+    "Average median renter income"
+] = df_high_loss_areas['SE_T093_003'].mean()
 
 
 entire_sample_results = {}
 
 entire_sample_results[
     "Total population at beginning of the period"
-] = df_census[f'totalpop{census_year}'].sum()
+] = df_census['SE_T001_001'].sum()
 
 
 entire_sample_results[
@@ -434,13 +453,44 @@ entire_sample_results[
 entire_sample_results[
     "Average change in density of a move"
 ] = (
-    df_moves['dest_fips'].map(census_col_by_area(f'popdens{census_year}')) \
-    - df_moves['orig_fips'].map(census_col_by_area(f'popdens{census_year}'))
+    df_moves['dest_fips'].map(census_col_by_area('SE_T002_002')) \
+    - df_moves['orig_fips'].map(census_col_by_area('SE_T002_002'))
 ).mean()
 
 
+entire_sample_results[
+    "Average median gross rent"
+] = df_census['SE_T167_001'].mean()
+
+entire_sample_results[
+    "Average median renter income"
+] = df_census['SE_T093_003'].mean()
+
+
 if export:
-    pass
+    prompt_width = max(map(len, area_results.keys())) + 2
+
+    with open(
+        f'./data/infutor+census/For_High-Loss_Areas_{infutor_start}-'
+        f'{infutor_end}.txt',
+        'w'
+    ) as f:
+        for k, v in area_results.items():
+            prompt = f'{k}: '
+            print(f'{prompt:<{prompt_width}}{v}', file=f)
+
+
+    prompt_width = max(map(len, entire_sample_results.keys())) + 2
+
+    with open(
+        f'./data/infutor+census/For_The_Entire_LA-OC_Sample_{infutor_start}'
+        f'-{infutor_end}.txt',
+        'w'
+    ) as f:
+        for k, v in entire_sample_results.items():
+            prompt = f'{k}: '
+            print(f'{prompt:<{prompt_width}}{v}', file=f)
+
 
 if verbose:
     with pd.option_context('display.max_columns', None):
